3c11e583c0fdc9e0278513117ec79725d845f238
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2024-11-25 14:57:40 +0530

    fix: removed used cart state in App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import "./App.css";
 import Cart from "./Cart";
 import ProductList from "./ProductList";
 import { PRODUCTS } from "./products";
-import { useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState([]);
-
   return (
     <div className="App">
       <h3>Welcome to the store</h3>
